3f1603a36be824d1dec193af26ce6b1f6086a986
LuisFHernandez <luisfelipehernandez2332@gmail.com>
2025-11-19 16:47:05 -0600

    Se agrega al print el estado de procedencia

--- hola.py
+++ hola.py
@@ -1,1 +1,1 @@
-print("¡Hola! Soy Luis, estudiante de desarrollo de software. Les mando saludos desde Texcoco.")
+print("¡Hola! Soy Luis, estudiante de desarrollo de software. Les mando saludos desde Texcoco, Estado de México.")
