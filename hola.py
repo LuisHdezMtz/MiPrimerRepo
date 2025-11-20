49c5abd77335e326ffb1cae7e358c4feca7e128d
LuisFHernandez <luisfelipehernandez2332@gmail.com>
2025-11-19 20:09:12 -0600

    Se imprime mensaje rama de prueba

--- hola.py
+++ hola.py
@@ -1,2 +1,3 @@
 print("¡Hola! Soy Luis, estudiante de desarrollo de software. Les mando saludos desde Texcoco, Estado de México.")
 print ("Estamos presentando una evidencia de aprendizaje, para la UVEG, saludos")
+print ("Estamos en rama de pruebas")
