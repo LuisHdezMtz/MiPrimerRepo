d4b258a1d79b7fde03094d25a4c42e4832dd00e1
LuisHdezMtz <email.no.existe.404@gmail.com>
2025-11-19 19:36:58 -0600

    Update hola.py
    Objetivo agregado

--- hola.py
+++ hola.py
@@ -1,1 +1,2 @@
 print("¡Hola! Soy Luis, estudiante de desarrollo de software. Les mando saludos desde Texcoco, Estado de México.")
+print ("Estamos presentando una evidencia de aprendizaje, para la UVEG, saludos")
